2d6b1ea4414635a4005431c69b0ad83b847b68d8
Sylvan Morris <sylvan.j.morris@gmail.com>
2019-08-09 21:13:02 +0200

    Completed the tutorial

--- Elm UI practice/src/Main.elm
+++ Elm UI practice/src/Main.elm
@@ -9,26 +9,102 @@ import Element.Font as Font
 import Element.Input as Input
 import Html exposing (Html)
 
-channelPanel : Element msg
-channelPanel =
-  column
-    [ height fill
-    , width <| fillPortion 1
-    , Background.color <| rgb255 92 99 118
-    , Font.color <| rgb255 255 255 255
-    ]
-    [ text "channels" ]
+channelPanel : List String -> String -> Element msg
+channelPanel channels activeChannel =
+    let
+        activeChannelAttrs =
+            [ Background.color <| rgb255 117 179 201, Font.bold ]
 
-chatPanel : Element msg
-chatPanel =
-  column [ height fill, width <| fillPortion 5 ]
-    [ text "chat" ]
+        channelAttrs =
+            [ paddingXY 15 5, width fill ]
+
+        channelEl channel =
+            el
+                (if channel == activeChannel then
+                    activeChannelAttrs ++ channelAttrs
+                 else
+                    channelAttrs
+                )
+            <|
+                text ("# " ++ channel)
+    in
+    column
+        [ height fill
+        , width <| fillPortion 1
+        , paddingXY 0 10
+        , Background.color <| rgb255 92 99 118
+        , Font.color <| rgb255 255 255 255
+        ]
+    <|
+        List.map channelEl channels
+
+type alias Message =
+    { author : String, time : String, text : String }
+
+chatPanel : String -> List Message -> Element msg
+chatPanel channel messages =
+    let
+        header =
+            row
+                [ width fill
+                , paddingXY 20 5
+                , Border.widthEach { bottom = 1, top = 0, left = 0, right = 0 }
+                , Border.color <| rgb255 200 200 200
+                ]
+                [ el [] <| text ("#" ++ channel)
+                , Input.button
+                    [ padding 5
+                    , alignRight
+                    , Border.width 1
+                    , Border.rounded 3
+                    , Border.color <| rgb255 200 200 200
+                    ]
+                    { onPress = Nothing
+                    , label = text "Search"
+                    }
+                ]
+        messageEntry message =
+            column [ width fill, spacingXY 0 5 ]
+                [ row [ spacingXY 10 0 ]
+                    [ el [ Font.bold ] <| text message.author, text message.time ]
+                , paragraph [] [ text message.text ]
+                ]
+
+        messagePanel =
+            column [ padding 10, spacingXY 0 20, scrollbarY ] <|
+                List.map messageEntry messages
+
+        footer =
+            el [ alignBottom, padding 20, width fill ] <|
+                row
+                    [ spacingXY 2 0
+                    , width fill
+                    , Border.width 2
+                    , Border.rounded 4
+                    , Border.color <| rgb255 200 200 200
+                    ]
+                    [ el
+                        [ padding 5
+                        , Border.widthEach { right = 2, left = 0, top = 0, bottom = 0 }
+                        , Border.color <| rgb255 200 200 200
+                        , mouseOver [ Background.color <| rgb255 86 182 139 ]
+                        ]
+                      <|
+                        text "+"
+                    , el [ Background.color <| rgb255 255 255 255 ] none
+                    ]
+    in
+    column [ height fill, width <| fillPortion 5 ]
+        [ header
+        , messagePanel
+        , footer
+        ]
 
 
 main : Html msg
 main =
   layout [] <|
     row [ height fill, width fill ]
-      [ channelPanel
-      , chatPanel
+      [ channelPanel [ "Anny", "Alex" ] "Anny"
+      , chatPanel "Anny" [ Message "Anny" "12:00AM" "Hey, how's it going?"]
       ]
